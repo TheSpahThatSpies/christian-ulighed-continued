0226da3475a4f2ecd6514e695ac4391f08acfaab
TheSpahThatSpies <christianborggaard@gmail.com>
2022-11-25 10:14:37 +0100

    start på opret data

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,33 @@ app.get('/user/:id',(req,res)=>{
 	});
 });
 
+//opret data
+app.post('/user',(req,res)=>{
+	console.log(req.body,'createdata');
 
+	let userName = req.body.name;
+	let eMail = req.body.email;
+	let mb = req.body.mobile;
+
+	let qr = `insert into user(name,email,mobile)
+			  values('$(userName)','$(eMail)','$(mb)')`;
+
+	db.query(qr,(err,result)=>{
+		if(err){console.log(err);}
+		
+		if(result.length>0)
+		{
+			res.send({
+				message:'data inserted'
+			});
+		}else
+		{
+			res.send({
+				message:'wrong...'
+			});
+		}
+	});
+});
 
 
 app.use(cors());
