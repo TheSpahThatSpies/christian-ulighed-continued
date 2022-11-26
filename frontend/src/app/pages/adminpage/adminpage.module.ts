5d3d54d73cdd2add301ebff7bdf927f266122f82
TheSpahThatSpies <christianborggaard@gmail.com>
2022-11-26 21:32:08 +0100

    reorganisering real + route start

---
+++ frontend/src/app/pages/adminpage/adminpage.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AdminpagePageRoutingModule } from './adminpage-routing.module';
+
+import { AdminpagePage } from './adminpage.page';
+
+import { HeroComponent } from 'src/app/components/admin/hero/hero.component';
+import { NavComponent } from 'src/app/components/main/nav/nav.component';
+import { LightNavComponent } from 'src/app/components/main/light-nav/light-nav.component';
+import { QuoteComponent } from 'src/app/components/about/quote/quote.component';
+import { HowItHelpsComponent } from 'src/app/components/about/how-it-helps/how-it-helps.component';
+import { AboutUsTextboxComponent } from 'src/app/components/about/about-us-textbox/about-us-textbox.component';
+import { InfoWithImageComponent } from 'src/app/components/about/info-with-image/info-with-image.component';
+import { FaqComponent } from 'src/app/components/about/faq/faq.component';
+import { SecondaryFooterComponent } from 'src/app/components/main/secondary-footer/secondary-footer.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AdminpagePageRoutingModule
+  ],
+  declarations: [AdminpagePage,
+    HeroComponent,
+    NavComponent,
+    LightNavComponent,
+    QuoteComponent,
+    HowItHelpsComponent,
+    AboutUsTextboxComponent,
+    InfoWithImageComponent,
+    FaqComponent,
+    SecondaryFooterComponent]
+})
+export class AdminpagePageModule {}
